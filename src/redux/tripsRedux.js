c203a4891767ee92f6e977b4cebe6e99883f42ff
Joanna Polska <joanna19polska@gmail.com>
2020-03-14 21:32:49 +0100

    Routing and Filtr for Trips added

--- src/redux/tripsRedux.js
+++ src/redux/tripsRedux.js
@@ -11,28 +11,31 @@ export const getFilteredTrips = ({trips, filters}) => {
     output = output.filter(trip => pattern.test(trip.name));
   }
 
-  // TODO - filter by duration
+  // filter by duration
+  if (filters.duration.from && filters.duration.to) {
+    output = output.filter(trip => trip.days >= filters.duration.from && trip.days <= filters.duration.to);
+  }
 
-  // TODO - filter by tags
+  // filter by tags
+  if (filters.tags) {
+    const pattern = new RegExp(filters.tags, 'i');
+    output = output.filter(trip => pattern.test(trip.tags));
+  }
 
-  // TODO - sort by cost descending (most expensive goes first)
+  // sort by cost descending (most expensive goes first)
 
   return output;
 };
 
 export const getTripById = ({trips}, tripId) => {
-  const filtered = trips;
-
-  // TODO - filter trips by tripId
+  const filtered = trips.filter(trip => trip.id == tripId);
 
   console.log('filtering trips by tripId:', tripId, filtered);
   return filtered.length ? filtered[0] : {error: true};
 };
 
 export const getTripsForCountry = ({trips}, countryCode) => {
-  const filtered = trips;
-
-  // TODO - filter trips by countryCode
+  const filtered = trips.filter(trip => trip.country.code == countryCode);
 
   console.log('filtering trips by countryCode:', countryCode, filtered);
   return filtered.length ? filtered : [{error: true}];
