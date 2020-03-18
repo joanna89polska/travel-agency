718697ffe9c0f45d07174dff3bd594e88e6e37bd
Joanna Polska <joanna19polska@gmail.com>
2020-03-18 16:06:26 +0100

    Order Form components added

--- src/components/features/OrderForm/OrderForm.js
+++ src/components/features/OrderForm/OrderForm.js
@@ -2,14 +2,23 @@ import React from 'react';
 import {Row, Col} from 'react-flexbox-grid';
 import OrderSummary from '../../features/OrderSummary/OrderSummary';
 import PropTypes from 'prop-types';
+import pricieng from '../../../data/pricing.json';
+import OrderOption from '../../features/OrderOption/OrderOption';
 
-const OrderForm = props => (
-  <Row>
-    <Col xs={12}>
-      <OrderSummary cost={props.tripCost} options={props.options}/>
-    </Col>
-  </Row>
-);
+const OrderForm = props => {
+  return (
+    <Row>
+      {pricieng.map(option => (
+        <Col key={option.id} md={4}>
+          <OrderOption {...option} />
+        </Col>
+      ))}
+      <Col xs={12}>
+        <OrderSummary cost={props.tripCost} options={props.options}/>
+      </Col>
+    </Row>
+  );
+};
 
 OrderForm.propTypes = {
   tripCost: PropTypes.string,
