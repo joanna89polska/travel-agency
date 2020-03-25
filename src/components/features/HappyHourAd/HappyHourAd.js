472045af316c912546bdfed6397be9407965ea06
Joanna Polska <joanna19polska@gmail.com>
2020-03-25 03:37:54 +0100

    Test 1 and Test 2 Added for HappyHourAd

---
+++ src/components/features/HappyHourAd/HappyHourAd.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class HappyHourAd extends React.Component {
+  render() {
+    return (
+      <div>
+        <h3 className={'title'}></h3>
+        <p className={'promoDescription'}></p>
+      </div>
+    );
+  }
+}
+export default HappyHourAd; 
---
+++ src/components/features/HappyHourAd/HappyHourAd.test.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import HappyHourAd from './HappyHourAd';
+
+const select = {
+  title: '.title',
+  promoDescription: '.promoDescription',
+};
+
+describe('Component HappyHourAd', () => {
+  it('should render without crashing', () => {
+    const component = shallow(<HappyHourAd />);
+    expect(component).toBeTruthy();
+  });
+
+  // nowy test:
+
+  it('should render heading and description', () => {
+    const component = shallow(<HappyHourAd />);
+    expect(component.exists(select.title)).toEqual(true);
+    expect(component.exists(select.promoDescription)).toEqual(true);
+  });
+
+
+});
